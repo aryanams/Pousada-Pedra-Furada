b8ec0de975cbce6e2c9f19152316580a8246ae13
aryanams <aryanasantos0704@gmail.com>
2026-03-20 04:14:17 +0000

    “fix: Corrigido o nome do arquivo index.tsx para index.jsx e feito configurações adicionais

--- jest.config.js
+++ jest.config.js
@@ -1,15 +1,13 @@
 /** @type {import('jest').Config} */ 
 const config = { 
-testEnvironment: "jsdom", 
-setupFilesAfterEnv: ["<rootDir>/jest.setup.js"], 
-moduleNameMapper: { 
-// ignora importações de CSS, imagens etc. durante o teste 
-"\\.(css|less|scss|sass)$": "identity-obj-proxy", 
-"\\.(jpg|jpeg|png|gif|webp|svg)$": "<rootDir>/__mocks__/fileMock.js", 
-}, 
-testPathIgnorePatterns: ["<rootDir>/.next/", "<rootDir>/node_modules/"], 
-transform: { 
-"^.+\\.(js|jsx|ts|tsx)$": "babel-jest", 
-}, 
+  testEnvironment: "jsdom", 
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"], 
+  moduleNameMapper: { 
+    "\\.(css|less|scss|sass)$": "identity-obj-proxy" 
+  }, 
+  transform: { 
+    "^.+\\.(js|jsx|ts|tsx)$": "babel-jest" 
+  }, 
+  testPathIgnorePatterns: ["<rootDir>/.next/", "<rootDir>/node_modules/"] 
 }; 
 module.exports = config;
